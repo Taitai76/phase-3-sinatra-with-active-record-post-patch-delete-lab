02a4a1002d5980248dbc9bbb7e6e716f022e038f
Mohammed Taitai <mohammedtaitai@outlook.com>
2023-05-09 11:52:51 -0400

    commit tree 1f0496bebbe441682af7ca15d0f7b07c76d5d8ce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,14 @@ class ApplicationController < Sinatra::Base
     bakery.to_json(include: :baked_goods)
   end
 
+  patch '/bakeries/:id' do
+    review = Bakery.find(params[:id])
+    review.update(
+      name: params[:name]
+    )
+    review.to_json
+  end
+
   get '/baked_goods/by_price' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_goods = BakedGood.by_price
@@ -22,5 +30,21 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+ 
+  post '/baked_goods' do 
+    baked_good = BakedGood.create(name:params[:name], price:params[:price], bakery_id:params[:bakery_id])
+    baked_good.to_json
+  end
 
+  get '/baked_goods/:id' do 
+    baked_good = BakedGood.find(params[:id])
+    baked_good.to_json
+  end
+
+
+  delete '/baked_goods/:id' do 
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
 end
